bdc30f5999231c9e569804c8aa4d70329ec92023
s19270 <61821772+s19270@users.noreply.github.com>
2020-04-19 16:02:31 +0200

    commit

---
+++ cw_3/cw_3/DAL/MockDbService.cs
@@ -0,0 +1,26 @@
+﻿using cw_3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.DAL
+{
+    public class MockDbService : IDbService
+    {
+        public static IEnumerable<Student> _students;
+        static MockDbService()
+        {
+            _students = new List<Student>{ 
+                new Student{IdStudent = 1, FirstName = "Jan", LastName = "Kowalski"},
+                new Student{IdStudent = 2, FirstName = "Robert", LastName = "Lewandowski"},
+                new Student{IdStudent = 3, FirstName = "Geralt", LastName = "Riv"},
+                new Student{IdStudent = 4, FirstName = "Darth", LastName = "Vader"}
+            };
+        }
+        public IEnumerable<Student> GetStudents()
+        {
+            return _students;
+        }
+    }
+}
---
+++ cw_3/cw_3/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cw_3.Models
+{
+    public class Student
+    {
+        public int IdStudent { set; get; }
+
+        public string FirstName { set; get; }
+
+        public string LastName { set; get; }
+
+        public string IndexNumber { set; get; }
+    }
+}
